76d2c0fd7983e691e118c4846cb38d1f543f784f
Amos <amoshuke@qq.com>
2025-02-10 23:52:02 +0800

    Update: models gemini-exp-1114 -> gemini-2.0-flash

--- pkgs/issue_triage_bot/lib/src/gemini.dart
+++ pkgs/issue_triage_bot/lib/src/gemini.dart
@@ -5,11 +5,8 @@ class GeminiService {
   // Possible values for models: gemini-1.5-pro-latest, gemini-1.5-flash-latest,
   // gemini-1.0-pro-latest, gemini-1.5-flash-exp-0827.
   //
-  // TODO(Amos): 目前 models/gemini-1.5-flash-latest 有些问题
-  // （https://discuss.ai.google.dev/t/solved-gemini-models-overloading-with-token-windows-of-less-than-20/49579/7）
-  //  临时换成 实验性的 gemini-exp-1114
-  static const String classificationModel = 'models/gemini-exp-1114';
-  static const String summarizationModel = 'models/gemini-exp-1114';
+  static const String classificationModel = 'models/gemini-2.0-flash';
+  static const String summarizationModel = 'models/gemini-2.0-flash';
 
   final GenerativeModel _summarizeModel;
   final GenerativeModel _classifyModel;
